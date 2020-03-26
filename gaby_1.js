2b8e62b17559b0c4bcefda8292bce90cf429ee94
Federico <correas.federico@gmail.com>
2020-03-26 10:16:52 -0300

    Archivo gaby_1.js modificado

--- gaby_1.js
+++ gaby_1.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 console.log("1° archivo");
 console.log("modificación archivo 1 hecha por Bilal");
-=======
-console.log("1° archivo, modificado por gaby")
->>>>>>> a46842c60bbe209ef2c841e3d7dad3fbc501338d
+console.log("1° archivo, modificado por gaby");
+console.log("1° modificación de Fede");
